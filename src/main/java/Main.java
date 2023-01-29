7836b39aec5dc770bae3edc85ff7891dafbdc95b
Дмитрий Князев <knyazevdi87@yandex.ru>
2023-01-29 13:34:27 +0300

    add gitignore, add try with resurse

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,35 +9,38 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import java.io.IOException;
 import java.util.List;
 
+import static java.lang.System.out;
+
 public class Main {
     public static final String REMOTE_URI = "https://raw.githubusercontent.com/netology-code/" +
             "jd-homeworks/master/http/task1/cats";
 
     public static ObjectMapper mapper = new ObjectMapper();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
                         .setSocketTimeout(30000)    // максимальное время ожидания получения данных
                         .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
                         .build())
-                .build();
+                .build()) {
 
         HttpGet request = new HttpGet(REMOTE_URI);
         CloseableHttpResponse response = httpClient.execute(request);
 
-        List<Fakt> fakts = mapper.readValue(
-                response.getEntity().getContent(),
-                new TypeReference<>(){
-                }
-        );
+            List<Fakt> fakts = mapper.readValue(
+                    response.getEntity().getContent(),
+                    new TypeReference<>() {
+                    }
+            );
 
-        fakts.stream().filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
-                .forEach(System.out::println);
+            fakts.stream().filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
+                    .forEach(System.out::println);
 
-        response.close();
-        httpClient.close();
+        } catch (IOException ex) {
+            out.println(ex.getMessage());
+        }
     }
 }
